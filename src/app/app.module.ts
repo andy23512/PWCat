8a717d1b6762759c24e278b7fe6df7244d1260c2
andy23512 <andy23512@gmail.com>
2018-08-11 22:08:18 +0800

    generate img-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImgCardComponent],
   imports: [BrowserModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
