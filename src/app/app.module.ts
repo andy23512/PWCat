b60a4dcac0c9bda7bde5323f2fea734254826028
andy23512 <andy23512@gmail.com>
2018-08-11 22:19:42 +0800

    import mat-button-module, add refresh button, add styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule, MatCardModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
   declarations: [AppComponent, ImgCardComponent],
-  imports: [BrowserModule, MatToolbarModule, MatCardModule],
+  imports: [BrowserModule, MatToolbarModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
